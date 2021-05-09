1a6c4f591d3c557ef1f120e8282d41df6f3ae503
Jonathan Niess <niessj@gmail.com>
2021-05-08 22:14:23 -0500

    API tweeking

--- src/App.js
+++ src/App.js
@@ -17,16 +17,16 @@ import Houston from "./Components/Cities/Houston";
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `https://obscure-basin-88823.herokuapp.com/https://api.printify.com/v1/shops.json`
+  baseURL: `https://obscure-basin-88823.herokuapp.com/https://api.printify.com/v1/shops/2861059/products.json`
 })
-
+/*2861059*/
 const options = {
   headers: {'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImRlZTc0NDU5Y2FhZGQ1YjUwYjExNGZhNDk0NmRlNWI5NGE4NjMxNjc5ZTEwYzYyODYyMGMxZjY2ZTFhOGVhODY4ZDU0NGU4MTQwN2NhMTI5In0.eyJhdWQiOiIzN2Q0YmQzMDM1ZmUxMWU5YTgwM2FiN2VlYjNjY2M5NyIsImp0aSI6ImRlZTc0NDU5Y2FhZGQ1YjUwYjExNGZhNDk0NmRlNWI5NGE4NjMxNjc5ZTEwYzYyODYyMGMxZjY2ZTFhOGVhODY4ZDU0NGU4MTQwN2NhMTI5IiwiaWF0IjoxNjE5OTMxOTU4LCJuYmYiOjE2MTk5MzE5NTgsImV4cCI6MTY1MTQ2Nzk1OCwic3ViIjoiNjczMzE0MyIsInNjb3BlcyI6WyJzaG9wcy5tYW5hZ2UiLCJzaG9wcy5yZWFkIiwiY2F0YWxvZy5yZWFkIiwib3JkZXJzLnJlYWQiLCJvcmRlcnMud3JpdGUiLCJwcm9kdWN0cy5yZWFkIiwicHJvZHVjdHMud3JpdGUiLCJ3ZWJob29rcy5yZWFkIiwid2ViaG9va3Mud3JpdGUiLCJ1cGxvYWRzLnJlYWQiLCJ1cGxvYWRzLndyaXRlIiwicHJpbnRfcHJvdmlkZXJzLnJlYWQiXX0.AsqvnqCSTMUvBbOzmeCwYX465bCqR8avfhXoxJCtrx3KSjN4eIYt1wyd0NJmwybABZoE229rUcJTGEaDCng'}
 };
 
 export default class App extends Component {
   
-  constructor() {
+  constructor() { 
     super();
     api.get('/', options).then(res => {
       console.log(res.data)
